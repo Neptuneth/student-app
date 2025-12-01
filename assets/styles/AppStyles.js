4c127849fcb3102124d241766f04a66e1535e179
Neptuneth <kaewkeiyrti@gmail.com>
2025-12-01 17:26:17 +0700

    first commit

---
+++ assets/styles/AppStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+ const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+    },
+    content:{
+        padding: 15,
+    }
+ })
+
+ export default styles;
---
+++ data/appinfo.js
@@ -0,0 +1,8 @@
+const appinfo = {
+    title: "StudentCares",
+    subtitle: "หิวข้าว",
+    schoolName: "NVC",
+    version: "1.0.0.",
+};
+
+export default appinfo;
